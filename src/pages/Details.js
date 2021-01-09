63c8361ece0a2cbaad003502b32c19f129b4fca8
vicnx <andanivicente@gmail.com>
2021-01-09 10:42:23 +0100

    add author things on event details

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonIcon, IonButton } from '@ionic/react';
-import { person, compass, alarm } from 'ionicons/icons';
+import { person, compass, alarm,star } from 'ionicons/icons';
 import ExploreContainer from '../components/ExploreContainer';
 import { useParams } from "react-router";
 import events from '../data/data.json';
@@ -33,17 +33,27 @@ const Details= () => {
           <div className="event-card">
             <div className="event-card-image">
               <img src={event.image}></img>
+              <div className="event-card-image-badges">
+                  <span className="event-card-image-badges-time badge-details badge-details-blue"><span className="badge-details-icon"><IonIcon icon={alarm}/></span><span>{event.time}</span></span>
+                  <span className="event-card-image-badges-location badge-details badge-details-green"><span className="badge-details-icon"><IonIcon icon={compass}/></span><span>{event.location.country}</span></span>
+                  <span className="event-card-image-badges-location badge-details badge-details-green"><span className="badge-details-icon"><IonIcon icon={compass}/></span><span>{event.location.postalcode}</span></span>
+                  <span className="event-card-image-badges-location badge-details badge-details-green"><span className="badge-details-icon"><IonIcon icon={compass}/></span><span>{event.location.city}</span></span>
+                </div>
             </div>
             <div className="event-card-content">
               <div className="event-card-content-left">
                 <span className="event-card-content-left-title">{event.title}</span>
-                <div className="event-card-content-left-badges">
-                  <span className="event-card-content-left-badges-time badge-details badge-details-blue"><span className="badge-details-icon"><IonIcon icon={alarm}/></span><span>{event.time}</span></span>
-                  <span className="event-card-content-left-badges-location badge-details badge-details-green"><span className="badge-details-icon"><IonIcon icon={compass}/></span><span>{event.location.country}</span></span>
-                  <span className="event-card-content-left-badges-location badge-details badge-details-green"><span className="badge-details-icon"><IonIcon icon={compass}/></span><span>{event.location.postalcode}</span></span>
-                  <span className="event-card-content-left-badges-location badge-details badge-details-green"><span className="badge-details-icon"><IonIcon icon={compass}/></span><span>{event.location.city}</span></span>
-                </div>
                 <span className="event-card-content-left-desc">{event.description}</span>
+                <div className="event-card-content-left-authorinfo">
+                  <div className="event-card-content-left-authorinfo-left">
+                    <img src={event.author.image}/>
+                  </div>
+                  <div className="event-card-content-left-authorinfo-right">
+                      <span className="event-card-content-left-authorinfo-right-username">{event.author.username}</span>
+                      <span className="event-card-content-left-authorinfo-right-bio">{event.author.bio}</span>
+                      <span className="event-card-content-left-authorinfo-right-rate"><span className="event-card-content-left-authorinfo-right-rate-icon"><IonIcon icon={star}/></span> {event.author.rate}/10</span>
+                  </div>
+                </div>
                 <IonButton className="event-card-content-left-join" color="success">JOIN</IonButton>
               </div>
               <div className="event-card-content-right">
